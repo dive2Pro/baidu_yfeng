efd1096bbdceb3f4b3f63111e4caac60a3e98057
huang yi chuan <toylucklebo@gmail.com>
2017-02-28 15:08:03 +0800

    finish 43

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ const dpv = new DatePickerView(document, 'datePiker');
 dpv.addSelectedListener(function () {
     console.log('addSele-------', arguments)
 });
+
+new MultiDatePickerView(document,'multidatePiker');

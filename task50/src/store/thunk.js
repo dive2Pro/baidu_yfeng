4e4042e734ae0033d37c91c1e369cbbb160cf74c
chuancey <toylucklebo@gmail.com>
2017-03-15 21:53:04 +0800

    refacotr(mobx):delete redux

--- task50/src/store/thunk.js
+++
@@ -1,9 +0,0 @@
-export default function(store) {
-  return next => action => {
-    if (typeof action !== 'function') {
-      next(action);
-    } else {
-      action(store.dispatch, store.getState);
-    }
-  };
-}
